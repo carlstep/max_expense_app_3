df96ca989d6c1c2f283d9a71e19b3152efb50ad1
carlstep <carlstep333@me.com>
2022-11-10 08:06:45 +0700

    73 combining widgets

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,52 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Chart Placeholder'),
+            ),
+          ),
+          Card(
+            child: Text('Transaction List Placeholder'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+// #70 - overview of project
+// #71 - overview of core flutter widgets
+// #72 - planning the app
+// #73 - combining widgets 
+/// Setup of new project, planning the layout, create placeholder widgets, 
+/// working with card widgets, understanding how parent/child dependecies work.
+/// How container widgets work with child and parent widgets.
+/// Card widget depends on the size of the child, unless the parent widget
+/// has its own defined width, then the card takes the width of the parent. 
+/// Column will take the width as its widest child.
