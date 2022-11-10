ce3c9223e12cf8037ad4baebd247fa4768df6c69
carlstep <carlstep333@me.com>
2022-11-10 08:17:37 +0700

    75 transaction model & data

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,18 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
             width: double.infinity,
             child: Card(
+              color: Colors.lightBlue,
               child: Text('Chart Placeholder'),
             ),
           ),
           Card(
+            color: Colors.red,
             child: Text('Transaction List Placeholder'),
           ),
         ],
@@ -37,18 +41,3 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-// #70 - overview of project
-// #71 - overview of core flutter widgets
-// #72 - planning the app
-// #73 - combining widgets 
-/// Setup of new project, planning the layout, create placeholder widgets, 
-/// working with card widgets, understanding how parent/child dependecies work.
-/// How container widgets work with child and parent widgets.
-/// Card widget depends on the size of the child, unless the parent widget
-/// has its own defined width, then the card takes the width of the parent. 
-/// Column will take the width as its widest child.
